ad10b33a70dcd7220d6cc6bdd177cee780602825
hashlag <90853356+hashlag@users.noreply.github.com>
2025-08-23 00:36:39 +0300

    Create basic file structure for Arc4 implementation

---
+++ Chaos/Cipher/Arc4.hpp
@@ -0,0 +1,9 @@
+#ifndef CHAOS_CIPHER_ARC4_HPP
+#define CHAOS_CIPHER_ARC4_HPP
+
+namespace Chaos::Cipher::Arc4
+{
+
+} // namespace Chaos::Cipher::Arc4
+
+#endif
---
+++ ChaosTests/Cipher/Arc4Tests.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+#include "Cipher/Arc4.hpp"
+
+using namespace Chaos::Cipher::Arc4;
