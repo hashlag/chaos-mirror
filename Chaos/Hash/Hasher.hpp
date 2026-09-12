a070a0c343ca0661120278a26f96400bf092ff9a
hashlag <hashlag@hashlag.local>
2026-09-12 03:11:05 +0300

    Update concept Hasher: require T::BLOCK_SIZE_BYTES to be a constant expression.

--- Chaos/Hash/Hasher.hpp
+++ Chaos/Hash/Hasher.hpp
@@ -14,7 +14,10 @@ template<typename T>
 concept Hasher = requires(T hasher, uint8_t * begin, uint8_t * end)
 {
     typename T::HashType;
+
     requires std::unsigned_integral<std::remove_cvref_t<decltype(T::BLOCK_SIZE_BYTES)>>;
+    typename std::integral_constant<decltype(T::BLOCK_SIZE_BYTES), T::BLOCK_SIZE_BYTES>;
+
     hasher.Reset();
     hasher.Update(begin, end);
     { hasher.Finish() } -> Hash;
